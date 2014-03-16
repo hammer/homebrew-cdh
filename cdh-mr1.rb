3391e844b180c07f62d5f1e982215ef02b5f3327
Anatoly Fayngelerin <fanatoly@gmail.com>
2014-03-16 10:38:05 -0400

    setting default value for JAVA_HOME using /usr/libexec/java_home

--- cdh-mr1.rb
+++ cdh-mr1.rb
@@ -15,6 +15,16 @@ class CdhMr1 < Formula
     (bin/'rcc').unlink
 
     inreplace "#{libexec}/conf/hadoop-env.sh",
+     "# export JAVA_HOME=/usr/lib/j2sdk1.6-sun",
+     "
+if [ \"$JAVA_HOME\" != \"\" ]; then
+  JAVA_HOME=$JAVA_HOME
+else
+  JAVA_HOME=$(/usr/libexec/java_home)
+fi
+     "
+
+    inreplace "#{libexec}/conf/hadoop-env.sh",
       "export HADOOP_IDENT_STRING=$USER",
       "
 export HADOOP_IDENT_STRING=$USER
